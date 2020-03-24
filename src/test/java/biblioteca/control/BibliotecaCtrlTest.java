e68b2a2ef2fc72258cb435f600610c2fffcad07e
Denis <denis.pascu.dp98@gmail.com>
2020-03-24 18:24:05 +0200

    Initial project

---
+++ src/test/java/biblioteca/control/BibliotecaCtrlTest.java
@@ -0,0 +1,22 @@
+package biblioteca.control;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BibliotecaCtrlTest {
+    //F01
+    @Test
+    public void adaugaCarte() {
+    }
+
+    //F02
+    @Test
+    public void cautaCarte() {
+    }
+
+    //F03
+    @Test
+    public void getCartiOrdonateDinAnul() {
+    }
+}
